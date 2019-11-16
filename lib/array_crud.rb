81957cd4c743faf818496c99da4a90d53233a443
Merkeresseria Coleman <kttsmllc@gmail.com>
2019-11-16 17:54:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,21 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  puppies = ["bulldog", "terrier", "poodle", "pittbull"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array)
+ puppies = ["bulldog", "terrier", "poodle", "pittbull"]
+ puppies << "pittdog"
+ puts puppies.inspect
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  puppies = ["bulldog", "terrier", "poodle"]
+ puppies.unshift("pittdog")
+ puts puppies.inspect
 end
 
 def remove_element_from_end_of_array(array)
