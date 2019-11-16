724fa8495ec018023af142b3e097e3a83bbc2c2a
Merkeresseria Coleman <kttsmllc@gmail.com>
2019-11-16 17:58:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array)
- puppies = ["bulldog", "terrier", "poodle", "pittbull"]
+ puppies = ["bulldog", "terrier",]
  puppies << "pittdog"
  puts puppies.inspect
  
